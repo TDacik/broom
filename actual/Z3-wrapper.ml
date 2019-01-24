6889d8e68e0596fab72eadc02a1b8b4a89df1db7
rogalew <rogalew@fit.vutbr.cz>
2019-01-24 16:28:00 +0100

    Z3-wrapper

--- actual/Z3-wrapper.ml
+++ actual/Z3-wrapper.ml
@@ -15,6 +15,7 @@ type sl_function_names_z3 = {
 	base : FuncDecl.func_decl;
 	len : FuncDecl.func_decl;
 	freed : FuncDecl.func_decl;
+	field : FuncDecl.func_decl;
 	}
 
 let get_sl_functions_z3 ctx =
@@ -23,10 +24,12 @@ let get_sl_functions_z3 ctx =
 	  base=FuncDecl.mk_func_decl ctx (Symbol.mk_string ctx "base") [(Integer.mk_sort ctx)] (Integer.mk_sort ctx);
 	  len=FuncDecl.mk_func_decl ctx (Symbol.mk_string ctx "len") [(Integer.mk_sort ctx)] (Integer.mk_sort ctx);
 	  freed=FuncDecl.mk_func_decl ctx (Symbol.mk_string ctx "freed") [(Integer.mk_sort ctx)] (Boolean.mk_sort ctx);
+	  field=FuncDecl.mk_func_decl ctx (Symbol.mk_string ctx "field") [(Integer.mk_sort ctx)] (Boolean.mk_sort ctx);
 	}
 
 
 
+(* Pure part translation into Z3 solver internam representation *)
 
 let const_to_solver ctx c=
 	match c with
@@ -63,13 +66,56 @@ let rec expr_to_solver ctx func expr =
 	    | Exp.Void ->  Integer.mk_numeral_i ctx (-1)
 	    | Exp.Undef -> Integer.mk_numeral_i ctx (-2)
 
-let rec pi_to_solver ctx pi =
-	let names_z3=get_sl_functions_z3 ctx in
+(* Global restrictions for base/len/size functions *)
+
+let create_global_restrictions ctx names_z3 =
+	(* forall x: (x - base(x)) + len(x) = len( base(x)) *)
+	let x=Quantifier.mk_bound ctx 0 (Integer.mk_sort ctx) in
+	let l=Arithmetic.mk_add ctx [ (Arithmetic.mk_sub ctx [x;(Expr.mk_app ctx names_z3.base [x] )]); (Expr.mk_app ctx names_z3.len [x]) ] in
+	let r=Expr.mk_app ctx names_z3.len [ (Expr.mk_app ctx names_z3.base [x]) ] in
+	let eq = Boolean.mk_eq ctx l r in
+	let q=Quantifier.mk_forall ctx [(Integer.mk_sort ctx)] [ (Symbol.mk_string ctx "x")] eq 
+		(Some 1) [] [] (Some (Symbol.mk_string ctx "Q1")) (Some (Symbol.mk_string ctx "skid1")) in
+	[ Quantifier.expr_of_quantifier q]
+
+(* create conditions to guarantee SL * validity ... "x-> ... * y -> ..." => x !=y *)
+
+let rec spatial_pred_to_solver ctx sp_pred1 rest_preds func =
+	let alloc x=(expr_to_solver ctx func x) in
+	let noneq_rule al sp_rule = (* create a nonequality al != x, where x is the allocated nod in sp_rule *)
+		match sp_rule with 
+		| Hpointsto (aa, _) ->
+			Boolean.mk_not ctx (Boolean.mk_eq ctx al (alloc aa))
+	in
+	match sp_pred1 with
+	| Hpointsto (a, _) -> (		
+		let field= Expr.mk_app ctx func.field [(alloc a)] in
+		let rec create_noneq to_parse =
+			match to_parse with
+			| first:: rest -> (noneq_rule (alloc a) first) :: create_noneq rest
+			| [] -> []
+		in
+		field :: create_noneq rest_preds
+		)
+
+(* Creation of the Z3 formulae for a SL formulae *)
+
+let rec pi_to_solver ctx names_z3 pi =
 	match pi with 
-		| [] -> []
-		| first::rest -> (expr_to_solver ctx names_z3 first) :: (pi_to_solver ctx rest)
+	| [] -> []
+	| first::rest -> (expr_to_solver ctx names_z3 first) :: (pi_to_solver ctx names_z3 rest)
 
+let rec sigma_to_solver ctx names_z3 sigma =
+	match sigma with
+	| [] -> []
+	| first::rest -> List.append (spatial_pred_to_solver ctx first rest names_z3) (sigma_to_solver ctx names_z3 rest)
 
+let formula_to_solver ctx form =
+	let names_z3=get_sl_functions_z3 ctx in
+	let pi= pi_to_solver ctx names_z3 form.pi in
+	let sigma= sigma_to_solver ctx names_z3 form.sigma in
+	let global_constr=create_global_restrictions ctx names_z3 in
+	List.append pi (List.append sigma global_constr)
 
 (* Experiments *)
 
