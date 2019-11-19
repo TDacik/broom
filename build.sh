39bd46fd13422c8af444b909e60bb407bb91ad09
versokova <ver.sokova@gmail.com>
2019-11-19 01:16:49 +0100

    code-listener: JSON dumper + build scripts

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# only for gcc, without dependencies
+
+# number of processor units
+NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
+
+export GCC_HOST="${GCC_HOST:-/usr/bin/gcc}"
+
+(
+    cd code-listener
+
+    status_update "Trying to build JSON dumper"
+    make -C json
+
+    status_update "Checking whether JSON dumper works"
+    make -C json check CTEST="ctest -j${NCPU}" \
+         || true
+#        || die "JSON dumper does not work"
+
+    status_update "Copying ATD"
+    make -C json atd
+    mkdir -p ../src/CL
+    cp json_build/*.ml* ../src/cl
+
+)
