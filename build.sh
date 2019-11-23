018277ab7e87ae2937bf1eb314af0ef95171eeb6
versokova <ver.sokova@gmail.com>
2019-11-23 01:27:31 +0100

    build.sh: missing status_update fnc

--- build.sh
+++ build.sh
@@ -2,25 +2,29 @@
 
 # only for gcc, without dependencies
 
+status_update() {
+    printf "\n%s...\n\n" "$*"
+    tty >/dev/null && printf "\033]0;%s\a" "$*"
+}
+
 # number of processor units
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
 
 export GCC_HOST="${GCC_HOST:-/usr/bin/gcc}"
 
-(
-    cd code-listener
+cd code-listener
 
-    status_update "Trying to build JSON dumper"
-    make -C json
+status_update "Trying to build JSON dumper"
+make -C json
 
-    status_update "Checking whether JSON dumper works"
-    make -C json check CTEST="ctest -j${NCPU}" \
-         || true
-#        || die "JSON dumper does not work"
+status_update "Checking whether JSON dumper works"
+make -C json check CTEST="ctest -j${NCPU}" \
+     || true
+#    || die "JSON dumper does not work"
 
-    status_update "Copying ATD"
-    make -C json atd
-    mkdir -p ../src/cl
-    cp json_build/*.ml* ../src/cl
+status_update "Copying ATD"
+make -C json atd
+mkdir -p ../src/cl
+cp json_build/*.ml* ../src/cl
 
-)
+cd ..
