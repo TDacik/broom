f52c9723abbd173bbfbea6541496da71c0a5e6ec
versokova <ver.sokova@gmail.com>
2020-04-20 05:47:53 +0200

    build - number of jobs

--- build.sh
+++ build.sh
@@ -9,21 +9,22 @@ status_update() {
 
 # number of processor units
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
+MAKE="make -j${NCPU} -s"
 
 export GCC_HOST="${GCC_HOST:-/usr/bin/gcc}"
 
 cd code-listener
 
 status_update "Trying to build JSON dumper"
-make -C json
+$MAKE -C json
 
 status_update "Checking whether JSON dumper works"
-make -C json check CTEST="ctest -j${NCPU}" \
+$MAKE -C json check \
      || true
 #    || die "JSON dumper does not work"
 
 status_update "Copying ATD"
-make -C json atd
+$MAKE -C json atd
 mkdir -p ../src/CL
 cp json_build/*.ml* ../src/CL
 
