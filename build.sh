fc3859d5308c1998f164c1b88a05c006844cfef2
versokova <ver.sokova@gmail.com>
2020-04-21 21:49:58 +0200

    build.sh: add clean and more checks

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+export SELF="$0"
 
 # only for gcc, without dependencies
+
+die() {
+    printf "%s: %s\n" "$SELF" "$*" >&2
+    exit 1
+}
 
 status_update() {
     printf "\n%s...\n\n" "$*"
@@ -13,14 +19,25 @@ MAKE="make -j${NCPU} -s"
 
 export GCC_HOST="${GCC_HOST:-/usr/bin/gcc}"
 
+# check GCC_HOST
+test -x "$GCC_HOST" || die "GCC_HOST is not an executable file: $GCC_HOST"
+
+# check code-listener source code
+[ "$(ls -A code-listener)" ] \
+    || die "Missing code-listener source code"
+
 cd code-listener
+
+status_update "Nuking working directory"
+$MAKE distclean \
+    || die "'$MAKE distclean' has failed"
 
 status_update "Trying to build JSON dumper"
 $MAKE -C json
 
 status_update "Checking whether JSON dumper works"
 $MAKE -C json check \
-     || true
+    || true
 #    || die "JSON dumper does not work"
 
 status_update "Copying ATD"
