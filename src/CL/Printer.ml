106d4460d41c421cd7ad585ea2deeb5fc9729f69
versokova <ver.sokova@gmail.com>
2020-04-29 20:08:39 +0200

    CL/Printer: fix print function declaration

--- src/CL/Printer.ml
+++ src/CL/Printer.ml
@@ -124,10 +124,10 @@ and constant_to_string data accs =
 let get_fnc_name f = operand_to_string f.Fnc.def
 
 let print_fnc_declaration f =
-	if CL.Util.is_fnc_static f then Printf.printf "static ";
+	if Util.is_fnc_static f then Printf.printf "static ";
 	let str = get_fnc_name f in
 		Printf.printf "%s(" str;
-		CL.Util.print_list var_to_string f.args;
+		Util.print_list var_to_string f.args;
 		Printf.printf ")"
 
 (* Print unary CL instruction *)
