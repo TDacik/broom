68390cc39d1181ef74bcb2f96cd40b6d60b1c2fb
versokova <ver.sokova@gmail.com>
2020-08-24 09:03:42 +0200

    tests: undetected memory leak Undef-(8)->ret

--- tests/easy-10b-err.c
+++ tests/easy-10b-err.c
@@ -6,10 +6,9 @@ struct sll_node {
 	struct sll_node *next;
 };
 
-int main() {
+struct sll_node * add() {
 	struct sll_node *p = malloc(sizeof(struct sll_node));
 	p->next = malloc(sizeof(struct sll_node));
 	p = p->next; // memory leak rewrite
-	free(p);
-	return 0;
+	return p;
 }
