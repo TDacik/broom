d1db060d83bf2ee44184abfd6d5d9e71223ff9e4
versokova <ver.sokova@gmail.com>
2020-10-05 14:08:30 +0200

    tests: typo

--- tests/easy-15-err.c
+++ tests/easy-15-err.c
@@ -7,9 +7,8 @@ int main(void) {
 	int* p = array;
 	int* q = array + 5;
 
-	ptrdiff_t qp= q - p;  // Valid. diff1 is 5
+	ptrdiff_t qp = q - p;  // Valid. diff1 is 5
 
-	ptrdiff_t pqa= p - q;  // Valid. diff2 is -5
-
+	ptrdiff_t pq = p - q;  // Valid. diff2 is -5
 
 }
