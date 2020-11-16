47d8b8494b28f90b1820afa22d76086de890612a
rogalew <rogalew@fit.vutbr.cz>
2020-11-16 14:47:55 +0100

    Abduction: split rule optimization

--- src/biabduction/Abduction.ml
+++ src/biabduction/Abduction.ml
@@ -217,9 +217,17 @@ let check_split_left {ctx=ctx; solv=solv; z3_names=z3_names} form1 i1 form2 i2 l
   if ((lhs=ff)||(rhs=ff))
   then false
   else
-  (*let query_null=[ expr_to_solver ctx z3_names (BinOp (Pneq, lhs_dest, Exp.zero));
-    (Boolean.mk_and ctx (formula_to_solver ctx form1))
-    ] in*)
+  let query_eq_base_sat = [
+	(Boolean.mk_eq ctx
+            	(Expr.mk_app ctx z3_names.base [rhs])
+              	(Expr.mk_app ctx z3_names.base [lhs]));
+  	(Boolean.mk_and ctx (formula_to_solver ctx form1));
+  	(Boolean.mk_and ctx (formula_to_solver ctx form2))]
+  in
+  if (Solver.check solv query_eq_base_sat)=UNSATISFIABLE
+  then false
+  else
+
   match level with
   | 1 ->
     let query=[
@@ -273,10 +281,16 @@ let check_split_right {ctx=ctx; solv=solv; z3_names=z3_names} form1 i1 form2 i2 
   if ((lhs=ff)||(rhs=ff))
   then false
   else
-  (* we should check that the destination is NULL or UNDEF *)
-  (*let query_null=[ expr_to_solver ctx z3_names (BinOp (Pneq, rhs_dest, Exp.zero));
-    (Boolean.mk_and ctx (formula_to_solver ctx form2))
-    ] in*)
+  let query_eq_base_sat = [
+	(Boolean.mk_eq ctx
+            	(Expr.mk_app ctx z3_names.base [rhs])
+              	(Expr.mk_app ctx z3_names.base [lhs]));
+  	(Boolean.mk_and ctx (formula_to_solver ctx form1));
+  	(Boolean.mk_and ctx (formula_to_solver ctx form2))]
+  in
+  if (Solver.check solv query_eq_base_sat)=UNSATISFIABLE
+  then false
+  else
   match level with
   | 1 ->
     let query=[
