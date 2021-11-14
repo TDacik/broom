070e782aa3b49921222e2f1549cf14f72cda46fb
versokova <ver.sokova@gmail.com>
2021-11-14 22:30:41 +0100

    tests: unsound program by Florian

---
+++ tests/unsound-err.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+extern int __VERIFIER_nondet_int(void);
+
+struct item {
+	struct item *next;
+	struct item *prev;
+};
+
+struct item *f(struct item *x) {
+	if (__VERIFIER_nondet_int())
+		return x;
+	else {
+		struct item * y = x->next;
+		return y;
+    }
+}
+
+int main()
+{
+    struct item *dll = NULL;
+
+    struct item *next = f(dll);
+
+    return 0;
+}
