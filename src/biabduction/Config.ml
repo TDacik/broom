8cd1d9e272398ff2fcc9bbc7102862b330fa5be7
versokova <ver.sokova@gmail.com>
2021-12-05 15:09:57 +0100

    add documentation

--- src/biabduction/Config.ml
+++ src/biabduction/Config.ml
@@ -23,6 +23,7 @@ let steps = Finish (* --steps=n|s|f *)
 
 end
 
+(** {3 Statistics} *)
 
 type stat = {
   abstracts : int ref;
@@ -35,7 +36,7 @@ type stat = {
 
 let statistics = {abstracts = ref 0; entailments = ref 0; badrerun = ref 0; internals = ref 0; errs = ref 0; warns = ref 0 }
 
-(* --stats *)
+(** --stats *)
 let stats () = true
 
 let display_stats () =
@@ -49,7 +50,7 @@ let display_stats () =
   )
 
 
-(** Errors handling *)
+(** {3 Errors handling} *)
 
 (** type for location in source code: __POS__ *)
 type src_pos = CL.Msg.src_pos
@@ -69,16 +70,16 @@ let prerr_warn str loc =
 let prerr_note str loc =
   CL.Msg.note (str,loc)
 
-(** Options *)
+(** {3 Options} *)
 
-(* describe verbose level as follow:
-  0 no debug information
-  1 print name of analysed function
-  2 + print information about expensive operations: abstraction/entailment
-  3 + print contract, abduction info, and current state for every instruction
-  information are printing on stderr
+(** describe verbose level as follow:
+  - 0 no debug information
+  - 1 print name of analysed function
+  - 2 + print information about expensive operations: abstraction/entailment
+  - 3 + print contract, abduction info, and current state for every instruction
+      information are printing on stderr
 *)
-let verbose () = 2
+let verbose () = 1
 
 let debug1 str =
   if 1 <= verbose () then prerr_endline str
@@ -93,26 +94,26 @@ let debug3_string str =
   if 3 <= verbose () then (prerr_string str; flush stderr;)
 
 
-(* --main=<name> set name of entry function - initializing global variables and
-   expecting 0 or 2 arguments (argv, argc) *)
+(** --main=<name> set name of entry function - initializing global variables and
+    expecting 0 or 2 arguments (argv, argc) *)
 let main () = "main"
 
 (* --only-fnc=<name> set the name of the function that will be the only one to
-  be analyzed, except for the functions called by this function *)
+   be analyzed, except for the functions called by this function *)
 (* let only_fnc () = "main" *)
 
-(* if true preconditions of function will be rerun when nondeterminismus or
+(** if true preconditions of function will be rerun when nondeterminismus or
    abstraction happend or go through the loops *)
 let rerun () = true
 
-(* --oom / --out-of-memory unsuccesful heap allocation *)
+(** --oom / --out-of-memory unsuccesful heap allocation *)
 let oom = false
 
-(* --exit-leaks report memory leaks of static variables at the end of program
+(** --exit-leaks report memory leaks of static variables at the end of program
    (while executing a no-return function or main) *)
 let exit_leaks () = true
 
-(* --memory_leaks_as_errors report memory leaks as errors otherwise warnings *)
+(** --memory_leaks_as_errors report memory leaks as errors otherwise warnings *)
 let memory_leaks_as_errors () = false
 
 (* do not use Slseg (Singly-linked List Segment) abstraction *)
@@ -121,34 +122,38 @@ let memory_leaks_as_errors () = false
 (* do not use Dlseg (Double-linked List Segment) abstraction *)
 (* let disable_dls = false *)
 
-(* EXPERIMENTAL: close lambdas within the abstraction *)
+(** EXPERIMENTAL: close lambdas within the abstraction *)
 let close_lambda () = false
 
-(* using of Slseg (Singly-linked List Segment) and Dlseg (Double-linked List
-  Segment) abstraction:
-  0 - disabeled
-  1 - after entailment unssucced
-  2 - before entailment *)
+(** using of Slseg (Singly-linked List Segment) and Dlseg (Double-linked List
+   Segment) abstraction:
+   - 0 - disabeled
+   - 1 - after entailment unssucced
+   - 2 - before entailment *)
 let abstraction_mode () = 1
 
-(* additionally perform abstraction after each just completed call on caller's
+(** additionally perform abstraction after each just completed call on caller's
    side *)
 let abstract_on_call_done () = false
 
-(* if true entailment states when traversing a loop-closing edge,
+(** if true entailment states when traversing a loop-closing edge,
    else on each basic block entry *)
 let entailment_on_loop_edges_only () = true
 
-(* max number of entailment calls for one loop : 0 - no limit *)
+(** max number of entailment calls for one loop : 0 - no limit *)
 let entailment_limit () = 5
 
-(* Abduction strategy: 0 - single strategy = one result,
-                       1 - more strategies = possible more restults
+(** Abduction strategy:
+    - 0 - single strategy = one result,
+    - 1 - more strategies = possible more restults
 *)
 let abduction_strategy = 0
 
-(* Solver timeout (in miliseconds) : 0 - no timeout *)
+(** Solver timeout (in miliseconds) : 0 - no timeout *)
 let solver_timeout = 2000
+
+(** Solver timeout for symplify states (in miliseconds) : 0 - no timeout *)
 let solver_timeout_simplify = 100
+
+(** Solver timeout for abstraction (in miliseconds) : 0 - no timeout *)
 let solver_timeout_abstraction = 200
-                   
