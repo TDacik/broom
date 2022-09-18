a51e39675312c1bccc19ac7de5a361ba3e79ed01
dk dk <kaimic1998@gmail.com>
2022-09-18 17:55:08 +0200

    added 2 abstraction-testcases

---
+++ src/tests/slseg-nonshared-2.ml
@@ -0,0 +1,29 @@
+(* Testcase building a formula with nodes containg the linking field and a data element
+*)
+(* EXPECTED: abstraction yields 
+%l4 -(8)->Undef * Slseg(%l1, %l3, lambda-1:2, [%l4])
+---------------
+lambda-1:2 [%l1, %l2, %l5, ] = %l1 -(8)->%l2 * (%l1+8) -(8)->%l5
+*)
+
+
+open Biabd
+open Formula
+
+open Biabd.Z3wrapper
+
+let () =
+	let ptr_size=Exp.Const (Int 8L) in
+	let solv=config_solver () in
+
+	let form = {
+		sigma = [Hpointsto(BinOp (Pplus, Var 1, ptr_size), ptr_size, Const(Int 1L));
+				 Hpointsto(BinOp (Pplus, Var 2, ptr_size), ptr_size, Const(Int 1L));
+				 Hpointsto(Var 1, ptr_size,Var 2); 
+				 Hpointsto(Var 2, ptr_size, Var 3);];
+		pi =	[BinOp ( Peq, Var 1, UnOp (Base, BinOp (Pplus, Var 1, ptr_size)));
+				 BinOp ( Peq, Var 2, UnOp (Base, BinOp (Pplus, Var 2, ptr_size)));
+				]
+	} in
+	let result = Abstraction.lseg_abstraction solv form [] in 
+	print_with_lambda result;
